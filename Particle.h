96a9a97e8c0ab66a4464407f614578690bf1edbf
Ben Lowe <79094373+BenLowe2003@users.noreply.github.com>
2025-04-15 09:39:55 +0100

    Update Particle.h
    Implemented interaction and fixed a compilation issue

--- Particle.h
+++ Particle.h
@@ -1,6 +1,10 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include <cmath> // For sqrt
+
+const float G = 9.81f; // Gravitational constant
+
 class Vector3D {
 public:
     float x, y, z;
@@ -44,13 +48,27 @@ public:
     Particle(const Vector3D& pos = Vector3D(), const Vector3D& vel = Vector3D(), float m = 1.0f)
         : position(pos), velocity(vel), mass(m) {}
 
-};
 
-    void integrate(float dt){
-        position += dt * velocity;
-        Vector3D acceleration = force * (1 / mass)
-        velocity += dt * acceleration;
-        force = 0;
+
+    void integrate(float dt) {
+        position = position + (velocity * dt); // Update position
+        Vector3D acceleration = force * (1.0f / mass); // Calculate acceleration
+        velocity = velocity + (acceleration * dt); // Update velocity
+        force = Vector3D(0.0f, 0.0f, 0.0f); // Reset force to zero vector
     }
+
+	void interaction(int i, const std::vector<Particle>& particles) {
+		for (int j = 0; j < particles.size(); j++) {
+			if (i != j) {
+				Vector3D r = particles[j].position - position; 
+				float square_distance = r.square_norm();
+				if (square_distance > 0.0f) {
+
+					force = force + (r * (particles[i].mass * particles[j].mass * G / square_distance * std::sqrt(square_distance)));
+				}
+			}
+		}
+	}
+};
 
 #endif // PARTICLE_H
