f21795d978b69286048c526d984d777bed03935a
Ben Lowe <79094373+BenLowe2003@users.noreply.github.com>
2025-04-16 13:45:50 +0100

    Update Main.cpp
    Appropriate dt for jupiters rings

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@
 int main() {
     // Create a simulation with 1000 particles
     int seed = static_cast<unsigned int>(time(0));
-    ParticleSimulation sim(5, 100, seed);
+    ParticleSimulation sim(5, 1000, seed);
 
     // Run the simulation
     sim.run(100);
