e33c7d2dd4d3d9df2c7f6357b1c6ae9c378d4c35
Ben Lowe <79094373+BenLowe2003@users.noreply.github.com>
2025-05-27 19:10:52 +0100

    Update ParticleSimulation.h

--- ParticleSimulation.h
+++ ParticleSimulation.h
@@ -4,26 +4,23 @@
 #include <vector>
 #include "Particle.h"
 
-constexpr size_t number_particles = 1000; // Number of particles in the simulation
+constexpr size_t number_particles = 500; // Number of particles in the simulation
 
 class ParticleSimulation {
 public:
 
     ParticleSimulation(float delta_time, unsigned int seed, float accretion_prob);
-
     void run(int steps);
+    void UpdatePositions();
+    float positions[3 * number_particles];
 
 private:
     void integrate();
     void interaction();
 
-	float positions[3 * number_particles]; // Array to store particle positions
-	float velocities[3 * number_particles]; // Array to store particle velocities
-	float forces[3 * number_particles]; // Array to store particle forces
-	float masses[number_particles]; // Array to store particle masses
+    
 
     std::vector<Particle> particles;
-    size_t number_particles;
     float delta_time;
     float accretion_probability;
 };
